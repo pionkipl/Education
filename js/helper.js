5096ee2f3f621cfb3a24c85016fd37a5830ee128
Jarek <szczepaniakdyfuzja@gmail.com>
2019-01-24 12:11:20 +0100

    Installing swiper, adding js helper and js swiper.

--- js/helper.js
+++ js/helper.js
@@ -1,7 +1,12 @@
 let helper = document.querySelector(".helper");
+let teacher = document.querySelectorAll(".teacher__item");
+let teacherMobile = document.querySelector(".teacher-mobile");
 
-window.addEventListener("resize", function() {
+window.addEventListener("resize", resize());
+
+function resize() {
   let w = window.innerWidth;
+  let hidden = document.querySelector(".hidden");
   if (w >= 1800) {
     helper.innerText = "big-desktop";
   } else if (w > 1000 && w <= 1200) {
@@ -15,4 +20,18 @@ window.addEventListener("resize", function() {
   } else {
     helper.innerText = "desktop";
   }
-});
+
+  if (w <= 767) {
+    hidden.style.display = "none";
+    teacherMobile.style.display = "flex";
+    teacher.forEach(function(el) {
+      el.style.justifyContent = "center";
+      el.style.paddingBottom = "3rem";
+    });
+  } else {
+    hidden.style.display = "flex";
+    teacherMobile.style.display = "none";
+  }
+}
+
+resize();
