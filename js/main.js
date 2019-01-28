0ea308ca571e1c4d44cc6ebee258da462f77f109
Jarek <szczepaniakdyfuzja@gmail.com>
2019-01-28 22:27:15 +0100

    Small improvements to subsribe section.

--- js/main.js
+++ js/main.js
@@ -11,3 +11,18 @@ function anim() {
   menuHamb.classList.toggle("hide");
   list.classList.toggle("open-menu");
 }
+
+// Validation
+let inputMail = document.querySelector(".subscribe__items-input");
+let subscribeButton = document.querySelector(".subscribe-button");
+subscribeButton.addEventListener("click", validateEmail);
+
+function validateEmail(e) {
+  let validation = /^\w+([\.-]?\w+)*@\w+([\.-]?)*(\.\w{2,20})+$/;
+  if (inputMail.value.match(validation)) {
+    inputMail.style.borderBottom = "1px solid green";
+  } else {
+    inputMail.style.borderBottom = "1px solid red";
+  }
+  e.preventDefault();
+}
