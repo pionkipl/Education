3729a29edcf49f9c6365fe936b86768a30b09147
pionkipl <jarek491@gmail.com>
2019-10-22 19:55:58 +0200

    Modified helper function.

--- js/helper.js
+++ js/helper.js
@@ -7,19 +7,19 @@ window.addEventListener("resize", resize);
 function resize() {
   let w = window.innerWidth;
   let hidden = document.querySelector(".hidden");
-  if (w >= 1800) {
-    helper.innerText = "big-desktop";
-  } else if (w > 1000 && w <= 1200) {
-    helper.innerText = "tab-land";
-  } else if (w > 767 && w <= 1000) {
-    helper.innerText = "tab-port";
-  } else if (w > 600 && w <= 767) {
-    helper.innerText = "phone-big";
-  } else if (w <= 600) {
-    helper.innerText = "phone";
-  } else {
-    helper.innerText = "desktop";
-  }
+  // if (w >= 1800) {
+  //   helper.innerText = "big-desktop";
+  // } else if (w > 1000 && w <= 1200) {
+  //   helper.innerText = "tab-land";
+  // } else if (w > 767 && w <= 1000) {
+  //   helper.innerText = "tab-port";
+  // } else if (w > 600 && w <= 767) {
+  //   helper.innerText = "phone-big";
+  // } else if (w <= 600) {
+  //   helper.innerText = "phone";
+  // } else {
+  //   helper.innerText = "desktop";
+  // }
 
   if (w <= 767) {
     hidden.style.display = "none";
