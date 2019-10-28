97bb562d12c2dc6b735bedf041fc74b721b36b2c
pionkipl <jarek491@gmail.com>
2019-10-28 07:49:40 +0100

    Fix mobile teacher link

--- js/helper.js
+++ js/helper.js
@@ -1,25 +1,13 @@
-let helper = document.querySelector(".helper");
-let teacher = document.querySelectorAll(".teacher__item");
-let teacherMobile = document.querySelector(".teacher-mobile");
+
+const teacher = document.querySelectorAll(".teacher__item");
+const teacherMobile = document.querySelector(".teacher-mobile");
+const myLinkTeachers = document.querySelector('.link-teachers');
 
 window.addEventListener("resize", resize);
 
 function resize() {
   let w = window.innerWidth;
   let hidden = document.querySelector(".hidden");
-  // if (w >= 1800) {
-  //   helper.innerText = "big-desktop";
-  // } else if (w > 1000 && w <= 1200) {
-  //   helper.innerText = "tab-land";
-  // } else if (w > 767 && w <= 1000) {
-  //   helper.innerText = "tab-port";
-  // } else if (w > 600 && w <= 767) {
-  //   helper.innerText = "phone-big";
-  // } else if (w <= 600) {
-  //   helper.innerText = "phone";
-  // } else {
-  //   helper.innerText = "desktop";
-  // }
 
   if (w <= 767) {
     hidden.style.display = "none";
@@ -37,4 +25,13 @@ function resize() {
   }
 }
 
+if (hidden.style.display === "none") {
+  myLinkTeachers.setAttribute('href', '#teachers-mobile');
+} else {
+  myLinkTeachers.setAttribute('href', '#teachers');
+}
+
 resize();
+
+
+
